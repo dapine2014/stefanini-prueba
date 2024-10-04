ebb5a23c58e370f1483cd9f1e72b0c4fdeb98183
alex <dapine@gmail.com>
2024-10-03 21:54:09 -0500

    Initial commit

---
+++ src/test/java/servico/rest/com/co/ServicioApplicationTests.java
@@ -0,0 +1,13 @@
+package servico.rest.com.co;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
