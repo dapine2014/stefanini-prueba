55a74c3eafb0adeb441044875721372ce2a59eea
alex <dapine@gmail.com>
2024-10-04 03:36:29 -0500

    logs

--- src/main/java/servico/rest/com/co/infraestructure/exception/GlobalExceptionHandler.java
+++ src/main/java/servico/rest/com/co/infraestructure/exception/GlobalExceptionHandler.java
@@ -1,16 +1,18 @@
 package servico.rest.com.co.infraestructure.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGlobalException(Exception ex) {
 
-        //logger.error("Error no controlado", ex);
+        log.error("Error no controlado", ex);
 
         return new ResponseEntity<>("Ha ocurrido un error en el servidor. Por favor, intenta nuevamente.", HttpStatus.INTERNAL_SERVER_ERROR);
     }
@@ -18,6 +20,7 @@ public class GlobalExceptionHandler {
     // Opcional: Puedes manejar excepciones más específicas si lo necesitas
     @ExceptionHandler(NullPointerException.class)
     public ResponseEntity<String> handleNullPointerException(NullPointerException ex) {
+        log.error("Referencia nula", ex);
         return new ResponseEntity<>("Ocurrió un error inesperado: referencia nula.", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
